e9d4f54fa21bd3fea3cde8fddfabaa0095f373ca
twledo <126789641+twledo@users.noreply.github.com>
2024-05-21 22:10:58 -0300

    Update descriptador.py

--- descriptador.py
+++ descriptador.py
@@ -2,8 +2,8 @@ def descriptador(senha, cont):
     senha_nova = ''
     for i in senha:
         numeros_letras = ord(i) + cont
-        # Se o valor estiver abaixo de 33, adicionamos 93 para envolvê-lo de volta dentro do intervalo 33-126
-        # Se estiver acima de 126, subtraímos 93 para trazê-lo de volta para dentro do intervalo
+        # se o valor estiver abaixo de 33, adiciono 93 para add de volta dentro do intervalo 33-126
+        # se estiver acima de 126, subtraío 93 para trazer de volta para dentro do intervalo
         while numeros_letras < 33:
             numeros_letras += 93
         while numeros_letras > 126:
@@ -11,9 +11,7 @@ def descriptador(senha, cont):
         senha_nova += chr(numeros_letras)
     return senha_nova
 
-# Solicitar a senha ao usuário
 senha = input("Digite a senha: ")
 
-# Loop de 1 a 92
 for cont in range(1, 93):
     print("Senha:", descriptador(senha, cont))
